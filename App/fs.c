90458519fb11a7f7f106f4ba674f436eb4f34fc3
yallawalla <nmocni4@guest.arnes.si>
2021-04-07 12:30:57 +0200

    iap

--- App/fs.c
+++ App/fs.c
@@ -26,75 +26,8 @@ int					AckWait(int t) {
 							if(n == nDev)
 								return EOF;
 						}
+						_print("...err");
 						return 0;
-}
-/*******************************************************************************
-* Function Name : str2hex
-* Description   : pretvorba stringa v hex stevilo
-* Input         : **p, pointer na string array (char *), n stevilo znakov
-* Output        : char * se poveca za stevilo znakov
-* Return        : hex integer
-*******************************************************************************/
-int					str2hex(char **p,int n) {
-char				q[16];
-						strncpy(q,*p,n)[n]='\0';
-						*p += n;
-						return(strtoul(q,NULL,16));
-						}
-/*******************************************************************************
-* Function Name  : HexChecksumError
-* Description    : preverja konsistentnost vrstice iz hex fila
-* Input          : pointer na string 
-* Return         : 0 ce je OK
-*******************************************************************************/
-int					HexChecksumError(char *p) {
-int	 				err,n=str2hex(&p,2);
-						for(err=n;n-->-5;err+=str2hex(&p,2));
-						return(err & 0xff);
-}
-/*******************************************************************************
-* Function Name  : CanHexProg request, server
-* Description    : dekodira in razbije vrstice hex fila na 	pakete 8 bytov in jih
-*								 : pošlje na CAN bootloader
-* Input          : pointer na string, zaporedne vrstice hex fila, <cr> <lf> ali <null> niso nujni
-* Output         : 
-* Return         : 0 ce je checksum error sicer eof(-1). bootloader asinhrono odgovarja z ACK message
-*				 				 : za vsakih 8 bytov !!!
-*******************************************************************************/
-int					CanHexProg(char *p) {
-static int	ExtAddr=0;
-int	 				n,a;
-payload 		pyld;
-
-						if(HexChecksumError(++p))
-							return 0;
-						n=str2hex(&p,2);
-						a=(ExtAddr<<16)+str2hex(&p,4);
-						switch(str2hex(&p,2)) {
-							case 00:
-								if(a<_FLASH_TOP)
-									return 0;
-								Send(_ID_IAP_ADDRESS,(payload *)&a,sizeof(int));
-								for(int i=0; n--;) {
-									pyld.byte[i++]=str2hex(&p,2);
-									if(i==8 || !n) {	
-										Send(_ID_IAP_DWORD,&pyld,i);
-										i=0;
-										if(!AckWait(200))
-											return 0;
-									}	
-								}
-								break;
-							case 01:
-								break;
-							case 02:
-								break;
-							case 04:
-							case 05:
-								ExtAddr=str2hex(&p,4);
-								break;
-						}
-						return(EOF);
 }
 /*******************************************************************************
 * Function Name	: 
@@ -133,15 +66,13 @@ FRESULT			iapRemote() {
 						_print("\r\nprogramming");
 						k=_FLASH_TOP;
 						Send(_ID_IAP_ADDRESS,(payload *)&k,sizeof(uint32_t));
+						_wait(10);
 						while(k <= n) {
-							_wait(2);
 							Send(_ID_IAP_DWORD,(payload *)k,sizeof(payload));
 							if(!AckWait(200))
 								return FR_NOT_READY;
-
 							if((k-_FLASH_TOP) % (8*((n-_FLASH_TOP)/8/20)) == 0)
 								_print(".%3d%c%c%c%c%c",(100*(k-_FLASH_TOP))/(n-_FLASH_TOP),'%','\x8','\x8','\x8','\x8');
-							
 							k+=sizeof(payload);
 						}
 						_print(".%3d%c%c%c%c%c",100,'%','\x8','\x8','\x8','\x8');
@@ -153,6 +84,7 @@ FRESULT			iapRemote() {
 						Send(_ID_IAP_GO,NULL,0);												// send <go> command
 						AckWait(0);
 						_print("and RUN :)\r\n");
+						DecodeCom(NULL);
 						return FR_OK;
 }
 /*******************************************************************************
